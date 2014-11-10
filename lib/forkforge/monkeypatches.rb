3163e66a29ebcba95066b451f3d030bba15dcd62
Alexei Matyushkin <am@mudasobwa.ru>
2014-11-10 07:00:29 +0100

    Fixed (backported Array.to_h) for 2.0.0.

--- lib/forkforge/monkeypatches.rb
+++ lib/forkforge/monkeypatches.rb
@@ -9,8 +9,26 @@ class Object
   end
 end
 
-class Hash
-  def take count, from = 0
-    Hash[self.to_a[from..from+count]]
+unless Hash.respond_to? take
+  class Hash
+    def take count, from = 0
+      Hash[self.to_a[from..from+count]]
+    end
+  end
+end
+
+unless Array.respond_to? to_h
+  class Array
+    def to_h
+      i = 0
+      self.inject({}) { |memo, e|
+        raise TypeError.new("wrong element type #{e.class} at #{i} (expected array)") unless Array === e
+        raise ArgumentError.new("wrong array length at #{i} (expected 2, was #{e.count})") unless e.count == 2
+        
+        i += 1
+        memo[e.first] = e.last
+        memo
+      }
+    end
   end
 end
