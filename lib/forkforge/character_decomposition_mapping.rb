a57ffc0d6d5d85421bedb6934cb56718ab197010
Alexei Matyushkin <am@mudasobwa.ru>
2014-11-10 06:34:45 +0100

    Character decompisition is preliminary handled.

---
+++ lib/forkforge/character_decomposition_mapping.rb
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+require_relative 'monkeypatches'
+require_relative 'unicode_data'
+
+module Forkforge
+  
+=begin
+  <font>    A font variant (e.g. a blackletter form).
+  <noBreak>   A no-break version of a space or hyphen.
+  <initial>   An initial presentation form (Arabic).
+  <medial>    A medial presentation form (Arabic).
+  <final>   A final presentation form (Arabic).
+  <isolated>    An isolated presentation form (Arabic).
+  <circle>    An encircled form.
+  <super>   A superscript form.
+  <sub>   A subscript form.
+  <vertical>    A vertical layout presentation form.
+  <wide>    A wide (or zenkaku) compatibility character.
+  <narrow>    A narrow (or hankaku) compatibility character.
+  <small>   A small variant form (CNS compatibility).
+  <square>    A CJK squared font variant.
+  <fraction>    A vulgar fraction form.
+  <compat>    Otherwise unspecified compatibility character.
+=end  
+  module CharacterDecompositionMapping
+    VARIANTS = {
+      :font     => { :character_name => "A font variant (e.g. a blackletter form)",  :general_category => "Yf" },
+      :noBreak  => { :character_name => "A no-break version of a space or hyphen",  :general_category => "Yn" },
+      :initial  => { :character_name => "An initial presentation form (Arabic)",  :general_category => "Yi" },
+      :medial   => { :character_name => "A medial presentation form (Arabic)",  :general_category => "Ym" },
+      :final    => { :character_name => "A final presentation form (Arabic)",  :general_category => "Yl" },
+      :isolated => { :character_name => "An isolated presentation form (Arabic)",  :general_category => "Ys" },
+      :circle   => { :character_name => "An encircled form",  :general_category => "Yc" },
+      :super    => { :character_name => "A superscript form",  :general_category => "Yu" },
+      :sub      => { :character_name => "A subscript form",  :general_category => "Yb" },
+      :vertical => { :character_name => "A vertical layout presentation form",  :general_category => "Yv" },
+      :wide     => { :character_name => "A wide (or zenkaku) compatibility character",  :general_category => "Yw" },
+      :narrow   => { :character_name => "A narrow (or hankaku) compatibility character",  :general_category => "Yr" },
+      :small    => { :character_name => "A small variant form (CNS compatibility)",  :general_category => "Ya" },
+      :square   => { :character_name => "A CJK squared font variant",  :general_category => "Yq" },
+      :fraction => { :character_name => "A vulgar fraction form",  :general_category => "Yt" },
+      :compat   => { :character_name => "Otherwise unspecified compatibility character",  :general_category => "Yo" }
+    }
+    VARIANTS_UC = VARIANTS.map { |k, v| [ "<#{k}>", v ] }.to_h
+     
+    extend self
+  end
+end
